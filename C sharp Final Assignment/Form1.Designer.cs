3ed544d9678eac7212ffbe1709fc4f713b0a394c
pmmrkumaradasa@students.nsbm.ac.lk <pmmrkumaradasa@students.nsbm.ac.lk>
2021-06-18 17:50:29 +0530

    heyyyyyy

--- C sharp Final Assignment/Form1.Designer.cs
+++ C sharp Final Assignment/Form1.Designer.cs
@@ -48,6 +48,7 @@ namespace C_sharp_Final_Assignment
             this.lblItemSalesCount = new System.Windows.Forms.Label();
             this.lblNetIncome = new System.Windows.Forms.Label();
             this.lblMostSoldItem = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
@@ -58,7 +59,7 @@ namespace C_sharp_Final_Assignment
             this.tableLayoutPanel1.ColumnCount = 3;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 41.36546F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 58.63454F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 337F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 254F));
             this.tableLayoutPanel1.Controls.Add(this.button1, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.label6, 0, 9);
             this.tableLayoutPanel1.Controls.Add(this.label5, 0, 8);
@@ -74,27 +75,30 @@ namespace C_sharp_Final_Assignment
             this.tableLayoutPanel1.Controls.Add(this.lblItemSalesCount, 1, 2);
             this.tableLayoutPanel1.Controls.Add(this.lblNetIncome, 1, 3);
             this.tableLayoutPanel1.Controls.Add(this.lblMostSoldItem, 1, 4);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Controls.Add(this.label9, 2, 6);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(9, 10);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 10;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 56.25F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 43.75F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 51F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 54F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 46F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 48F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 42F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 48F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 48F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(906, 482);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 41F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 44F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 37F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 39F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 34F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 41F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 39F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 39F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(680, 392);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(238, 3);
+            this.button1.Location = new System.Drawing.Point(178, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(327, 40);
+            this.button1.Size = new System.Drawing.Size(245, 32);
             this.button1.TabIndex = 0;
             this.button1.Text = "Generate Report";
             this.button1.UseVisualStyleBackColor = true;
@@ -103,27 +107,30 @@ namespace C_sharp_Final_Assignment
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(3, 433);
+            this.label6.Location = new System.Drawing.Point(2, 352);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(98, 17);
+            this.label6.Size = new System.Drawing.Size(74, 13);
             this.label6.TabIndex = 7;
             this.label6.Text = "Most sold item";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(3, 385);
+            this.label5.Location = new System.Drawing.Point(2, 313);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(79, 17);
+            this.label5.Size = new System.Drawing.Size(62, 13);
             this.label5.TabIndex = 6;
             this.label5.Text = "Net Income";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(3, 335);
+            this.label4.Location = new System.Drawing.Point(2, 272);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(110, 17);
+            this.label4.Size = new System.Drawing.Size(84, 13);
             this.label4.TabIndex = 5;
             this.label4.Text = "Item sales count";
             this.label4.Click += new System.EventHandler(this.label4_Click);
@@ -131,18 +138,20 @@ namespace C_sharp_Final_Assignment
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 94);
+            this.label1.Location = new System.Drawing.Point(2, 77);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(110, 17);
+            this.label1.Size = new System.Drawing.Size(84, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Item sales count";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(238, 248);
+            this.button2.Location = new System.Drawing.Point(178, 201);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(327, 42);
+            this.button2.Size = new System.Drawing.Size(245, 34);
             this.button2.TabIndex = 3;
             this.button2.Text = "Show previous month report";
             this.button2.UseVisualStyleBackColor = true;
@@ -151,18 +160,20 @@ namespace C_sharp_Final_Assignment
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 145);
+            this.label2.Location = new System.Drawing.Point(2, 118);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(79, 17);
+            this.label2.Size = new System.Drawing.Size(62, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Net Income";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(3, 199);
+            this.label3.Location = new System.Drawing.Point(2, 162);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(98, 17);
+            this.label3.Size = new System.Drawing.Size(74, 13);
             this.label3.TabIndex = 4;
             this.label3.Text = "Most sold item";
             this.label3.Click += new System.EventHandler(this.label3_Click_1);
@@ -170,18 +181,20 @@ namespace C_sharp_Final_Assignment
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(3, 53);
+            this.label7.Location = new System.Drawing.Point(2, 43);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(96, 17);
+            this.label7.Size = new System.Drawing.Size(72, 13);
             this.label7.TabIndex = 8;
             this.label7.Text = "Report Period";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(3, 293);
+            this.label8.Location = new System.Drawing.Point(2, 238);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(96, 17);
+            this.label8.Size = new System.Drawing.Size(72, 13);
             this.label8.TabIndex = 9;
             this.label8.Text = "Report Period";
             // 
@@ -192,55 +205,70 @@ namespace C_sharp_Final_Assignment
             this.chart1.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(588, 3);
+            this.chart1.Location = new System.Drawing.Point(442, 2);
+            this.chart1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.chart1.Name = "chart1";
             this.tableLayoutPanel1.SetRowSpan(this.chart1, 6);
             series1.ChartArea = "ChartArea1";
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(315, 238);
+            this.chart1.Size = new System.Drawing.Size(236, 193);
             this.chart1.TabIndex = 10;
             this.chart1.Text = "chart1";
             // 
             // lblReportPeriod
             // 
             this.lblReportPeriod.AutoSize = true;
-            this.lblReportPeriod.Location = new System.Drawing.Point(238, 53);
+            this.lblReportPeriod.Location = new System.Drawing.Point(178, 43);
+            this.lblReportPeriod.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblReportPeriod.Name = "lblReportPeriod";
-            this.lblReportPeriod.Size = new System.Drawing.Size(0, 17);
+            this.lblReportPeriod.Size = new System.Drawing.Size(0, 13);
             this.lblReportPeriod.TabIndex = 11;
             // 
             // lblItemSalesCount
             // 
             this.lblItemSalesCount.AutoSize = true;
-            this.lblItemSalesCount.Location = new System.Drawing.Point(238, 94);
+            this.lblItemSalesCount.Location = new System.Drawing.Point(178, 77);
+            this.lblItemSalesCount.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblItemSalesCount.Name = "lblItemSalesCount";
-            this.lblItemSalesCount.Size = new System.Drawing.Size(0, 17);
+            this.lblItemSalesCount.Size = new System.Drawing.Size(0, 13);
             this.lblItemSalesCount.TabIndex = 12;
             // 
             // lblNetIncome
             // 
             this.lblNetIncome.AutoSize = true;
-            this.lblNetIncome.Location = new System.Drawing.Point(238, 145);
+            this.lblNetIncome.Location = new System.Drawing.Point(178, 118);
+            this.lblNetIncome.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblNetIncome.Name = "lblNetIncome";
-            this.lblNetIncome.Size = new System.Drawing.Size(0, 17);
+            this.lblNetIncome.Size = new System.Drawing.Size(0, 13);
             this.lblNetIncome.TabIndex = 13;
             // 
             // lblMostSoldItem
             // 
             this.lblMostSoldItem.AutoSize = true;
-            this.lblMostSoldItem.Location = new System.Drawing.Point(238, 199);
+            this.lblMostSoldItem.Location = new System.Drawing.Point(178, 162);
+            this.lblMostSoldItem.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblMostSoldItem.Name = "lblMostSoldItem";
-            this.lblMostSoldItem.Size = new System.Drawing.Size(0, 17);
+            this.lblMostSoldItem.Size = new System.Drawing.Size(0, 13);
             this.lblMostSoldItem.TabIndex = 14;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(428, 238);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(85, 13);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Hey!! I pushed!!!";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(930, 506);
+            this.ClientSize = new System.Drawing.Size(698, 411);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Sales Report";
             this.tableLayoutPanel1.ResumeLayout(false);
@@ -268,6 +296,7 @@ namespace C_sharp_Final_Assignment
         private System.Windows.Forms.Label lblItemSalesCount;
         private System.Windows.Forms.Label lblNetIncome;
         private System.Windows.Forms.Label lblMostSoldItem;
+        private System.Windows.Forms.Label label9;
     }
 }
 
